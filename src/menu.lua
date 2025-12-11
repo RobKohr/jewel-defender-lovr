500722856fc45ce537277f9ce5d7115b6c309dc7
rkohr <robert.kohr@yahooinc.com>
2025-12-11 14:41:18 -0800

    create menu factory to easily create menus, move menu_screen to use that, create pause menu

---
+++ src/menu.lua
@@ -0,0 +1,122 @@
+local Menu = {}
+local Utils = require("src.utils")
+local Mouse = require("src.mouse")
+
+-- Cache frequently used functions
+local getNormalizedPosition = Mouse.getNormalizedPosition
+local drawHUDText = Utils.drawHUDText
+local debugMousePosition = Utils.debugMousePosition
+
+-- HUD text sizing (normalized units)
+local MENU_TEXT_SIZE = 0.136
+local MENU_ITEM_SPACING = 0.15
+local MENU_SHADOW_OFFSET = 0.0136
+
+-- Default menu starting Y position (normalized units)
+local DEFAULT_MENU_START_Y = -0.232
+
+-- Factory function to create a new menu instance
+function Menu.create()
+  local menu = {
+    menu_font = nil,
+    menu_items = {},
+    hovered_index = nil,
+    menu_start_x = nil,
+    menu_start_y = DEFAULT_MENU_START_Y,
+    show_debug_pointer = false,
+    show_debug_mouse_position = false,
+  }
+  
+  -- Initialize font
+  menu.menu_font = lovr.graphics.newFont('assets/fonts/Montserrat-ExtraBoldItalic.ttf', 80, 4)
+  
+  -- Set menu items
+  function menu:setMenuItems(menu_start_x, items)
+    self.menu_start_x = menu_start_x
+    self.menu_items = {}
+    
+    for i, item in ipairs(items) do
+      local y_pos = self.menu_start_y + (i - 1) * MENU_ITEM_SPACING
+      table.insert(self.menu_items, {
+        x = menu_start_x,
+        y = y_pos,
+        label = item.label,
+        callback = item.callback,
+      })
+    end
+  end
+  
+  -- Update hover state based on mouse position
+  function menu:update()
+    local mouse_x, mouse_y = getNormalizedPosition()
+    if not mouse_x or not mouse_y then
+      self.hovered_index = nil
+      return
+    end
+    
+    local x = mouse_x
+    self.hovered_index = nil
+    
+    for i, position in ipairs(self.menu_items) do
+      -- width is MENU_TEXT_SIZE * number of characters in the label
+      local labelWidth = 0.45
+      if mouse_y > position.y and mouse_y < position.y + MENU_TEXT_SIZE and 
+         x > -0.86 and x < position.x + labelWidth then
+        self.hovered_index = i
+        break
+      end
+    end
+    
+    -- Handle mouse clicks
+    if lovr.system.wasMousePressed(1) and self.hovered_index then
+      local menu_item = self.menu_items[self.hovered_index]
+      if menu_item and menu_item.callback then
+        menu_item.callback()
+      end
+    end
+  end
+  
+  -- Draw the menu
+  function menu:draw(pass)
+    for i, position in ipairs(self.menu_items) do
+      local is_hovered = (self.hovered_index == i)
+      local show_shadow = not is_hovered
+      local shadow_offset = show_shadow and MENU_SHADOW_OFFSET or nil
+      local y_pos = position.y
+      drawHUDText(pass, position.label, position.x, y_pos, MENU_TEXT_SIZE, 'left', 'top', self.menu_font, shadow_offset)
+    end
+    
+    if self.show_debug_mouse_position then
+      debugMousePosition(pass, self.menu_font)
+    end
+    
+    if self.show_debug_pointer then
+      local mouse_x, mouse_y = getNormalizedPosition()
+      if mouse_x and mouse_y then
+        local viewport_width, viewport_height = pass:getDimensions()
+        local aspect = viewport_width / viewport_height
+        local circle_size = (25 / viewport_height) * 2
+        
+        Utils.setupHUD(pass)
+        pass:setColor(1, 0, 0, 1)
+        pass:circle(mouse_x * aspect, mouse_y, 0, circle_size, 0, 1, 0, 0, 'fill')
+        pass:setColor(1, 1, 1, 1)
+        pass:pop()
+      end
+    end
+  end
+  
+  -- Set debug pointer visibility
+  function menu:setShowDebugPointer(show)
+    self.show_debug_pointer = show
+  end
+  
+  -- Set debug mouse position visibility
+  function menu:setShowDebugMousePosition(show)
+    self.show_debug_mouse_position = show
+  end
+  
+  return menu
+end
+
+return Menu
---
+++ src/screens/game/pause_menu.lua
@@ -0,0 +1,74 @@
+local PauseMenu = {}
+local Menu = require("src.menu")
+local Utils = require("src.utils")
+
+-- Cache frequently used functions
+local drawHUDBackground = Utils.drawHUDBackground
+
+local menu = nil
+PauseMenu.show = false
+
+-- Menu starting position (normalized units)
+local MENU_START_X = -0.867
+
+local function returnToGame()
+  PauseMenu.show = false
+end
+
+local function returnToMenu()
+  local Screen = require("src.screen")
+  PauseMenu.show = false
+  Screen.SetCurrentScreen("MenuScreen")
+end
+
+local function quit()
+  lovr.event.quit()
+end
+
+function PauseMenu.init()
+  -- Create menu instance
+  menu = Menu.create()
+  menu:setMenuItems(MENU_START_X, {
+    {label = "Return to Game", callback = returnToGame},
+    {label = "Return to Menu", callback = returnToMenu},
+    {label = "Quit", callback = quit},
+  })
+end
+
+function PauseMenu.update()
+  if PauseMenu.show and menu then
+    menu:update()
+  end
+end
+
+function PauseMenu.draw(pass)
+  if PauseMenu.show and menu then
+    -- Draw semi-transparent overlay
+    Utils.setupHUD(pass)
+    pass:setColor(0, 0, 0, 0.5)  -- Semi-transparent black overlay
+    local viewport_width, viewport_height = pass:getDimensions()
+    local aspect = viewport_width / viewport_height
+    pass:plane(0, 0, 0, aspect * 2, 2, 0, 0, 1, 0, 'fill')
+    pass:setColor(1, 1, 1, 1)
+    pass:pop()
+    
+    -- Draw menu on top
+    menu:draw(pass)
+  end
+end
+
+function PauseMenu.onKeyPressed(key, scancode, isrepeat, action)
+  if not PauseMenu.show then
+    return
+  end
+  
+  -- Handle escape to close pause menu
+  if key == "escape" then
+    PauseMenu.show = false
+    return true  -- Consume the key event
+  end
+  
+  return false
+end
+
+return PauseMenu
